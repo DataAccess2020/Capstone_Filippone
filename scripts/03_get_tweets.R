23a7c7162fb3ac0208e694c1417cdb83189af857
Lara Filippone <lara.filippone@studenti.unimi.it>
2022-12-19 15:47:04 +0100

    saves tweets

--- scripts/03_get_tweets.R
+++ scripts/03_get_tweets.R
@@ -2,5 +2,15 @@
 
 tweets_left<-get_timeline(user=left_pol$screen_name,
                           n=500, verbose=T, parse=T, Sys.sleep(1))
+
 tweets_right<-get_timeline(user=right_pol$screen_name, n=500,
                            verbose=T, parse=T, Sys.sleep(1))
+
+# And saving the two data frames.
+tweets_left_save<-sapply(tweets_left, as.character)
+write.csv(tweets_left_save, "tweets_left.csv",
+          row.names = F)
+
+tweets_right_save<-sapply(tweets_right, as.character)
+write.csv(tweets_right_save, "tweets_right.csv",
+          row.names = F)
