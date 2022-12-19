09bc2182778e9641786b4b1010bf061af3c495bf
Lara Filippone <lara.filippone@studenti.unimi.it>
2022-12-19 16:18:05 +0100

    again

--- scripts/03_get_tweets.R
+++ scripts/03_get_tweets.R
@@ -9,7 +9,7 @@ tweets_right<-get_timeline(user=right_pol$screen_name, n=500,
 # And saving the two data frames.
 tweets_left_save<-sapply(tweets_left, as.character)
 write.csv(tweets_left_save, "tweets_left.csv",
-          row.names = F)
+          row.names = F) 
 
 tweets_right_save<-sapply(tweets_right, as.character)
 write.csv(tweets_right_save, "tweets_right.csv",
