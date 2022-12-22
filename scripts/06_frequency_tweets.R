dcbfe6b07e1d1bebe1b9ef81be7651f13d6e7dc5
Lara Filippone <lara.filippone@studenti.unimi.it>
2022-12-22 23:28:07 +0100

    corrects script because I realized I had filtered tweets twice

--- scripts/06_frequency_tweets.R
+++ scripts/06_frequency_tweets.R
@@ -2,10 +2,7 @@
 # showing the frequency of tweets from left-wing politicians.
 library(ggplot2)
 
-tweets_left_2022<-tweets_left %>% dplyr::filter(created_at > "2022-01-01"
-                              & created_at <= "2022-12-22")
-
-freq_left<-ts_plot(tweets_left_2022, by = "months" )+
+freq_left<-ts_plot(tweets_left, by = "months" )+
   ggplot2::theme_minimal() +
   ggplot2::theme(plot.title = ggplot2::element_text(face = "bold")) +
   ggplot2::labs(x = NULL, y = NULL,
@@ -14,10 +11,7 @@ freq_left<-ts_plot(tweets_left_2022, by = "months" )+
 freq_left
 
 # We do the same for right-wing politicians.
-tweets_right_2022<-tweets_right%>%dplyr::filter(created_at>"2022-01-01"
-                                         & created_at<="2022-12-22")
-
-freq_right<-ts_plot(tweets_right_2022, by = "months" )+
+freq_right<-ts_plot(tweets_right, by = "months" )+
   ggplot2::theme_minimal() +
   ggplot2::theme(plot.title = ggplot2::element_text(face = "bold")) +
   ggplot2::labs(x = NULL, y = NULL,
