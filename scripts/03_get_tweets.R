b2f881f81341d74b2c9192bad89c606333a727aa
Lara Filippone <lara.filippone@studenti.unimi.it>
2022-12-22 15:30:16 +0100

    restricts to tweets from 2022

--- scripts/03_get_tweets.R
+++ scripts/03_get_tweets.R
@@ -1,10 +1,15 @@
-# Getting the 500 last tweets of every politician.
+# Getting the 500 last tweets of every politician, filtering for year
+# 2022.
 
 tweets_left<-get_timeline(user=left_pol$screen_name,
-                          n=500, verbose=T, parse=T, Sys.sleep(1))
+                          n=500, verbose=T, parse=T, Sys.sleep(1))%>%
+                          dplyr::filter(created_at>"2022-01-01"
+                                      & created_at<="2022-12-22")
 
 tweets_right<-get_timeline(user=right_pol$screen_name, n=500,
-                           verbose=T, parse=T, Sys.sleep(1))
+                           verbose=T, parse=T, Sys.sleep(1))%>%
+                          dplyr::filter(created_at>"2022-01-01"
+                                      & created_at<="2022-12-22")
    
 # And saving the two data frames.
 tweets_left_save<-sapply(tweets_left, as.character)
